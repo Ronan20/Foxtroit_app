354a23ee6d36d49b74e24a27f28e422f5e23167b
Ronan20 <ronanbmchugh@gmail.com>
2016-01-10 22:01:33 +0000

    added initial project

---
+++ FoxtroitApp/FoxtroitApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoxtroitApp
+//
+//  Created by Ronan McHugh on 10/01/2016.
+//  Copyright (c) 2016 Ronan McHugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
