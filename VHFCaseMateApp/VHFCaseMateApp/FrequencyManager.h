4934fdcde9e79983c3b4143e585e8e991e8d715b
Ronan20 <ronanbmchugh@gmail.com>
2016-03-13 22:32:38 +0000

    Initial code for device detection

---
+++ VHFCaseMateApp/VHFCaseMateApp/FrequencyManager.h
@@ -0,0 +1,16 @@
+//
+//  FrequencyManager.h
+//  
+//
+//  Created by Ronan McHugh on 13/03/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FrequencyManager : NSObject
+
+-(instancetype) init NS_UNAVAILABLE;
+
+
+@end
